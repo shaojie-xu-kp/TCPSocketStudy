18d6585d01914238c0831cd7970fc960aa12a477
shaojie.xu <shaojie.xu@infinitagroup.it>
2016-04-07 11:11:52 +0200

    first commit

---
+++ src/com/shaojie/www/MultiThreadedServer.java
@@ -0,0 +1,80 @@
+package com.shaojie.www;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiThreadedServer implements Runnable{
+
+    protected int          serverPort;
+    protected ServerSocket serverSocket = null;
+    protected boolean      isStopped    = false;
+    protected Thread       runningThread= null;
+
+    public MultiThreadedServer(int port){
+        this.serverPort = port;
+    }
+
+    public void run(){
+        synchronized(this){
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+        while(! isStopped()){
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if(isStopped()) {
+                    System.out.println("Server Stopped.") ;
+                    return;
+                }
+                throw new RuntimeException(
+                    "Error accepting client connection", e);
+            }
+            new Thread(
+                new WorkerRunnable(
+                    clientSocket, "Multithreaded Server")
+            ).start();
+        }
+        System.out.println("Server Stopped.") ;
+    }
+
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop(){
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open port : " + this.serverPort, e);
+        }
+    }
+    
+    public static void main(String... args){
+    	
+    	MultiThreadedServer server = new MultiThreadedServer(8080);
+    	new Thread(server).start();
+
+    	try {
+    	    Thread.sleep(200 * 1000);
+    	} catch (InterruptedException e) {
+    	    e.printStackTrace();
+    	}
+    	
+    	System.out.println("Stopping Server");
+    	server.stop();
+    }
+
+}
